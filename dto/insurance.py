5c058e400995a630c84a2c7db3ca0b320515e061
Readeem <99687127+Readeem@users.noreply.github.com>
2024-11-22 13:46:02 +0300

    Healthcheck fix

--- dto/insurance.py
+++ dto/insurance.py
@@ -14,7 +14,6 @@ class InsuranceRequest(BaseModel):
     cargo_type: CargoType
     declared_value: float
     date: datetime.date = Field(default_factory=datetime.date.today)
-    # timezone: datetime.timezone | None = None
 
 
 class InsuranceResponse(BaseModel):
